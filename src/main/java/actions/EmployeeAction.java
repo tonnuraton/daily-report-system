93658388f97fc8b96f22c8b818bb5785b5743c48
tonnura <masaki0224@icloud.com>
2024-11-08 01:04:49 +0900

    「従業員indexアクションの作成」

---
+++ src/main/java/actions/EmployeeAction.java
@@ -0,0 +1,60 @@
+package actions;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+
+import actions.views.EmployeeView;
+import constants.AttributeConst;
+import constants.ForwardConst;
+import constants.JpaConst;
+import services.EmployeeService;
+
+/**
+ * 従業員に関わる処理を行うActionクラス
+ *
+ */
+
+public class EmployeeAction extends ActionBase  {
+	private EmployeeService service;
+
+	/**
+     * メソッドを実行する
+     */
+	@Override
+	public void process() throws ServletException, IOException{
+
+		service = new EmployeeService();
+
+		invoke();
+
+		service.close();
+	}
+
+	/**
+     * 一覧画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+	public void index() throws ServletException, IOException{
+		int page = getPage();
+		List<EmployeeView> employees = service.getPerPage(page);
+
+		long employeeCount = service.countAll();
+
+		putRequestScope(AttributeConst.EMPLOYEES, employees);
+		putRequestScope(AttributeConst.EMP_COUNT, employeeCount); //全ての従業員データの件数
+		putRequestScope(AttributeConst.PAGE, page);
+		putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE);
+
+
+	String flush = getSessionScope(AttributeConst.FLUSH);
+	if (flush != null) {
+		putRequestScope(AttributeConst.FLUSH, flush);
+		removeSessionScope(AttributeConst.FLUSH);
+	}
+	forward(ForwardConst.FW_EMP_INDEX);
+
+}
+}
