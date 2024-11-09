09fa8d3cb4bf92314e1b4af71f0766d5e6e072a2
tonnura <masaki0224@icloud.com>
2024-11-09 17:27:58 +0900

    従業員editアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -123,4 +123,19 @@ public class EmployeeAction extends ActionBase  {
 
 	}
 
+	public void edit() throws ServletException, IOException {
+
+		EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+
+		if( ev == null || ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()) {
+			forward(ForwardConst.FW_ERR_UNKNOWN);
+			return;
+
+		}
+		putRequestScope(AttributeConst.TOKEN, getTokenId());
+		putRequestScope(AttributeConst.EMPLOYEE, ev);
+
+
+		forward(ForwardConst.FW_EMP_EDIT);
+	}
 }
